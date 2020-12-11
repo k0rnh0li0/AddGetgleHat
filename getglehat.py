8b984d496181af35b899136927fa402e263af06a
k0rnh0li0 <k0rnh0li0@protonmail.com>
2020-12-11 12:42:02 -0500

    Add eye tilt adjustment

--- getglehat.py
+++ getglehat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 
 import cv2
 import numpy as np
@@ -16,10 +17,14 @@ if __name__ == "__main__":
         exit()
 
     input_path = sys.argv[1]
-    output_path = sys.argv[2]
+    output_path = None
 
-    # load face classifier
+    if len(sys.argv) == 3:
+        output_path = sys.argv[2]
+
+    # load classifiers
     cascade_face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    cascade_eye = cv2.CascadeClassifier("haarcascade_eye.xml")
 
     # load images
     img = cv2.imread(input_path)
@@ -40,6 +45,7 @@ if __name__ == "__main__":
     # find faces
     faces = cascade_face.detectMultiScale(img_gray, 1.3, 5)
 
+    # add getgle hat to each face
     for (x,y,w,h) in faces:
         face_x1 = x
         face_x2 = x + w
@@ -49,10 +55,32 @@ if __name__ == "__main__":
         hat_width = int(w*1.2)
         hat_height = int(hat_width * hat_h / hat_w)
 
-        # find bounding rect for hat
-        hat_x1 = face_x2 - int(w/2) - int(hat_width/2.25)
+        # find slope between eyes to try to correct for head tilt
+        eye_tilt = 0
+        eye_roi = img[y:y+h,x:x+w]
+        eye_roi_gray = img_gray[y:y+h,x:x+w]
+        eyes = cascade_eye.detectMultiScale(eye_roi_gray)
+
+        EYE_ADJUST = 1
+
+        if len(eyes) == 2:
+            dx = eyes[1][0] - eyes[0][0]
+            dy = eyes[1][1] - eyes[0][1]
+            EYE_ADJUST = dx
+
+            if dx == 0:
+                eye_tilt = math.pi / 2
+            else:
+                eye_tilt = math.atan(dy / dx)
+
+        # find bounding rect for hat, adjusting for eye tilt
+        tilt_dx = int(math.cos(eye_tilt) * w/EYE_ADJUST)
+        tilt_dy = int(math.sin(eye_tilt) * h/EYE_ADJUST)
+
+        hat_x1 = face_x2 - int(w/2) - int(hat_width/2.25) + tilt_dx
+        hat_y1 = face_y1 - int(h*0.4) + tilt_dy
+
         hat_x2 = hat_x1 + hat_width
-        hat_y1 = face_y1 - int(h*0.4)
         hat_y2 = hat_y1 + hat_height
 
         # resize hat to head
@@ -67,4 +95,7 @@ if __name__ == "__main__":
 
         img[hat_y1:hat_y2,hat_x1:hat_x2] = dst
 
-    display_img(img)
+    if output_path == None:
+        display_img(img)
+    else:
+        cv2.imwrite(output_path, img)
